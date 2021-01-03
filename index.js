36ccc14ee348d655d735a868dea66a87ba4e2e6c
Luke Blaney <git@lukeblaney.co.uk>
2021-01-03 00:50:50 +0000

    add basic info endpoint for monitoring

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ const app = express();
 const port = process.env.PORT;
 if (!port) throw "no PORT environment variable set";
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
 	res.send("Hello World");
-})
+});
+
+app.get('/_info', (req, res) => {
+	const output = {
+		system: 'lucos_loganne',
+		checks: {
+		},
+		metrics: {},
+		ci: {
+			circle: "gh/lucas42/lucos_loganne",
+		}
+	};
+	res.send(output);
+});
 app.listen(port, function () {
   console.log('App listening on port ' + port);
 });
